e2f80147e87bb93b5a622daa2144b69823dd6cc3
Christian Danielsen <ckdanielsen@gmail.com>
2017-12-28 10:42:53 -0800

    spin up boilerplate; initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pdxnow
+
+An app that aggregates various channels to show what's happening in Portland, OR at any given moment
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">NOW</h1>
+        </header>
+        <section className="Main-content">
+
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
